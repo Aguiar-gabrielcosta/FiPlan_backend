108f283ca801b4e8696b6e177ecf914c7d31c431
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-07-20 16:27:25 -0300

    Adiciona o .env local

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TransactionModule } from './transaction/transaction.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [TransactionModule],
+  imports: [ConfigModule.forRoot(), TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
