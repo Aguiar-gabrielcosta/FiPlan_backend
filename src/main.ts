d4de05eed849c347121682ab1a218833c860f05c
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-07-23 19:31:08 -0300

    comentário

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  // Validação em todas as entradas de dados.
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.DEV_PORT)
 }
